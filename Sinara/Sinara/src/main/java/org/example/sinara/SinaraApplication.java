46658b5ca3119e956f644389921d93e478e8a3d0
Salla0905 <bruna.salla@germinare.org.br>
2025-08-11 10:37:24 -0300

    initial commit

---
+++ Sinara/Sinara/src/main/java/org/example/sinara/SinaraApplication.java
@@ -0,0 +1,13 @@
+package org.example.sinara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinaraApplication.class, args);
+    }
+
+}
---
+++ Sinara/Sinara/src/test/java/org/example/sinara/SinaraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sinara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
