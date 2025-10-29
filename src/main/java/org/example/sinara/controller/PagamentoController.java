0c8316a8e36fe8f1e16e002b3d2dbb782c9278cc
Lorenaatavares <157626068+Lorenaatavares@users.noreply.github.com>
2025-10-28 22:05:34 -0300

    fix/ds2-263/adicionando-classe-cors

--- src/main/java/org/example/sinara/controller/PagamentoController.java
+++ src/main/java/org/example/sinara/controller/PagamentoController.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/pagamento")
-@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class PagamentoController implements PagamentoOpenApi {
     private final PagamentoService pagamentoService;
 
