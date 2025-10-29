895578d952a3ef55764f7a8be863f3616117b0f4
RafaPizza3 <rafapitherpizza@gmail.com>
2025-10-28 23:34:18 -0300

    corrigindo classe de security para carregar o cors corretamente

--- src/main/java/org/example/sinara/security/SecurityConfig.java
+++ src/main/java/org/example/sinara/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/admin/**").hasAuthority("ROLE_Admin")
